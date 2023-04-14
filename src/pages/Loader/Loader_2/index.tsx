4b6059e4e27b305a95d21ad10a5d1dfa28210e81
BlueLanM <w2351406802@163.com>
2023-04-14 17:41:23 +0800

    add loader

---
+++ src/pages/Loader/Loader_2/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+function index() {
+  return (
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default index;
--- src/pages/Loader/index.tsx
+++ src/pages/Loader/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./index.css";
 import Loader_1 from "./Loader_1";
+import Loader_2 from "./Loader_2";
+import Loader_3 from "./Loader_3";
 
 function index() {
   const component = [
     { model: <Loader_1 />, color: "#fff" },
-    // { model: <Toggle_2 /> },
-    // { model: <Toggle_3 /> },
+    { model: <Loader_2 />, color: "#fff" },
+    { model: <Loader_3 /> },
     // { model: <Toggle_4 /> },
   ];
   return (
